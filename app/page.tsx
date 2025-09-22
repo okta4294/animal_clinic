30fed7ef4bb3f7fc5547ba2cbd7a0fb2bc5d412e
okta <fachrudinokta@gmail.com>
2025-09-22 22:07:56 +0700

    update

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { redirect } from 'next/navigation'
+import React from 'react'
+
+const  Page = async () => {
+  redirect('/login')
+    return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Page
